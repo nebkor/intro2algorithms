9ff6331050c1716868b32ecd139607bc03e585ae
Joe Ardent <code@ardent.nebcorp.com>
2016-07-17 23:24:24 -0500

    can sort a file of numbers via commandline

--- file_inversions.rkt
+++ file_inversions.rkt
@@ -1,40 +1,23 @@
 #lang racket
 
-(define (count-inversions lst)
-  (define (cnt l len)
-    (cond
-     [(= len 0) 0]
-     [(= len 1) 0]
-     [(= len 2) (if (< (car l) (cadr l))
-                    0
-                    1)]
-     [else
-      (if (even? len)
-          (let* ([half-len (/ len 2)]
-                 [right (take-right l half-len)]
-                 [left (drop-right l half-len)])
-            (+ (cnt left half-len)
-               (cnt right half-len)))
-          (+ (cnt (take-right l (sub1 len)) (sub1 len))
-             (cnt (drop-right l 1) (sub1 len))))]))
-  (cnt lst (length lst)))
-
-(define (merge l1 l2)
-  (define (help l1 l2 m)
-    (cond
-     [(and (null? l1) (null? l2)) m]
-     [(null? l1) (append m l2)]
-     [(null? l2) (append m l1)]
-     [else
-      (let ([c1 (car l1)]
-            [c2 (car l2)])
-        (if (> c1 c2)
-            (help l1 (cdr l2) (append m (list c2)))
-            (help (cdr l1) l2 (append m (list c1)))))]))
-  (help l1 l2 '()))
-
 (define (msort lst)
-  (define (help lst len)
+  ;; called by the main helper function
+  (define (merge l1 l2)
+    (define (help l1 l2 m)
+      (cond
+       [(and (null? l1) (null? l2)) m]
+       [(null? l1) (append m l2)]
+       [(null? l2) (append m l1)]
+       [else
+        (let ([c1 (car l1)]
+              [c2 (car l2)])
+          (if (> c1 c2)
+              (help l1 (cdr l2) (append m (list c2)))
+              (help (cdr l1) l2 (append m (list c1)))))]))
+    (help l1 l2 '()))
+
+  ;; now to recursively split and merge
+  (define (main-help lst len)
     (if (< len 2)
         lst
         (let* ([right-len (if (even? len)
@@ -43,6 +26,53 @@
                [left-len (- len right-len)]
                [right (take-right lst right-len)]
                [left (drop-right lst right-len)])
-          (merge (help left left-len)
-                 (help right right-len)))))
-  (help lst (length lst)))
+          (merge (main-help left left-len)
+                 (main-help right right-len)))))
+  ;; returns the sorted list
+  (main-help lst (length lst)))
+
+;; read in integer list like:
+;;
+;; (define nums
+;;   (map (lambda (x) (string->number x))
+;;        (call-with-input-file numfile
+;;          (lambda (in) (port->lines in)))))
+
+(define (count-inversions lst)
+  ;; called by the main helper function
+  (define (merge l1 l2)
+    (define (help l1 l2 m)
+      (cond
+       [(and (null? l1) (null? l2)) m]
+       [(null? l1) (append m l2)]
+       [(null? l2) (append m l1)]
+       [else
+        (let ([c1 (car l1)]
+              [c2 (car l2)])
+          (if (> c1 c2)
+              (help l1 (cdr l2) (append m (list c2)))
+              (help (cdr l1) l2 (append m (list c1)))))]))
+    (help l1 l2 '()))
+
+  ;; now to recursively split and merge
+  (define (main-help lst len)
+    (if (< len 2)
+        lst
+        (let* ([right-len (if (even? len)
+                              (/ len 2)
+                              (/ (sub1 len) 2))]
+               [left-len (- len right-len)]
+               [right (take-right lst right-len)]
+               [left (drop-right lst right-len)])
+          (merge (main-help left left-len)
+                 (main-help right right-len)))))
+  ;; returns the sorted list
+  (main-help lst (length lst)))
+
+(module+ main
+  (let* ([args (current-command-line-arguments)]
+         [numfile (vector-ref args 0)]
+         [nums (map (lambda (x) (string->number x))
+                    (call-with-input-file numfile
+                      (lambda (in) (port->lines in))))])
+    (msort nums)))
